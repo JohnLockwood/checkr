ae92a990ff48eeeebe809f1b61e3462afec09bd5
John Lockwood <John@Codesolid.com>
2021-01-29 16:44:51 -0500

    Basic support for name transposition etc.  Good!

--- lev.py
+++ lev.py
@@ -3,6 +3,11 @@ from typing import List
 
 
 def lev(str1, str2):
+
+    """ Levenstein match return 0 on exact match, one on one change, etc.  Note that some implementations
+        allow insert/update/delete to be scored separately.  Non-recursive implementations are least expensive
+        because you're only scanning both strings once.
+    """
     x_len = len(str1) + 1
     y_len = len(str2) + 1
     matrix = np.zeros((x_len, y_len))
@@ -31,30 +36,50 @@ def lev(str1, str2):
     return matrix[x_len - 1, y_len - 1]
 
 
-def find_in_list(needle: str, haystack: List[str], use_numpy: bool = True) -> str:
-    """Returns the first match with the lowest Levenshtein score (closest match)
+def find_in_list(needle: str, haystack: List[str], cutoff: int = 2) -> str:
+    """Returns the last match with the lowest Levenshtein score (closest match) that is at or below the cutoff value
+       If no match return an empty string.
 
-       An improvement could consider ties.
+       An improvement could consider ties and return a list.
+
+       Could also do other preprocessing such as case-insensitive compare (run upper on everything up front). "
     """
 
     assert haystack is not None
     assert None not in haystack
     assert str is not None
 
-    unset_high_score = 1000
-    lowest = unset_high_score
+    lowest = cutoff
     match = ''
-    for elem in haystack:
-        if use_numpy:
-            score = lev(needle, elem)
-        else:
-            score = lev_no_numpy(needle, elem)
+    for i, elem in enumerate(haystack):
+        score = lev(needle, elem)
 
-        if score < lowest:
+        if score <= lowest:
             lowest = score
             match = elem
 
     return match
+
+
+def remove_punctuation(source: str) -> str:
+    assert str is not None
+    return source.replace(",", "").replace(".", "").replace(";", "")
+
+
+def normalize_name(source: str) -> str:
+    """transforms a name into a sorted list of uppercase tokens, with punctuation removed"""
+    name = remove_punctuation(source).upper()
+    tokens = name.split(" ")
+    tokens.sort()
+    return " ".join(tokens)
+
+
+def find_in_list_with_transposition_support(needle: str, haystack: List[str], cutoff: int = 2) -> str:
+    names = []
+    for name in haystack:
+        names.append(normalize_name(name))
+    return find_in_list(normalize_name(needle), names, cutoff)
+
 
 
 def lev_no_numpy(str1, str2):
--- test_lev_pytest.py
+++ test_lev_pytest.py
@@ -1,19 +1,49 @@
 
-from lev import lev, find_in_list
+from lev import *
 
 
-def test_one_change():
+l_test = ["Jack Lockwood", "Jeff Roberts", "Len Roberts", "Mable Lackwood"]
+s_test = "John Lockwood"
+
+
+def test_known_score():
     assert lev('foot', 'fool') == 1
 
 
 def test_low_score_returns_lowest_scored():
-    s_test = "John Lockwood"
-    l_test = ["Jack Lockwood", "Jeff Roberts", "Len Roberts", "Mable Lackwood"]
-    matched = find_in_list(s_test, l_test, False)
+    matched = find_in_list(s_test, l_test, 3)
     assert matched == "Jack Lockwood"
 
-    # Now try with exact match
+
+def test_no_match_if_below_cutoff():
+    # Exact match works fine
+    matched = find_in_list(s_test, l_test)
+    assert matched == ""
+
+
+def test_exact_match_works_by_default():
+    # Exact match works fine
     l_test.append(s_test)
-    matched = find_in_list(s_test, l_test, False)
+    matched = find_in_list(s_test, l_test)
     assert matched == s_test
+    l_test.pop()
+    assert s_test not in l_test
+
+
+def test_normalize_name_removes_punctuation():
+    n = normalize_name("Mr. John Jones, Esq.")
+    assert "." not in n
+    assert "," not in n
+
+
+def test_normalize_sorts_and_uppercases():
+    n = normalize_name("Jenniffer Cajal")
+    assert n == "CAJAL JENNIFFER"
+
+
+def test_normalized_match_works():
+    l_test_local = l_test.copy()
+    l_test_local.append("Lockwood, John")
+    m = find_in_list_with_transposition_support(s_test, l_test_local)
+    assert m == normalize_name(s_test)
 
